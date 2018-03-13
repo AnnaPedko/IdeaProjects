8ad4054473390a7091d82b762db2c67ee29b04fc
Anna Khorolets <andreevna.ne@gmail.com>
2018-03-13 02:51:59 +0200

    Added changes to main class

--- Task_1/src/main/java/Task_1.java
+++ Task_1/src/main/java/Task_1.java
@@ -5,10 +5,10 @@ public class Task_1
     public static void main(String[] args)
     {
         int intArray[] = {1000, -4, 12, 2, 3 , 10, 50, 7};
-
-        ArrayUtil.mergeSort(intArray);
-        //ArrayUtil.bubbleSort(intArray);
-       // ArrayUtil.quickSort(intArray);
+        int quickSortArray[] = {1000, -4, 10, 92, 3 , 11, 50, 7};
+        //ArrayUtil.mergeSort(intArray);
+        ArrayUtil.bubbleSort(intArray);
+        ArrayUtil.quickSort(quickSortArray);
         System.out.println(ArrayUtil.findMax(intArray));
         System.out.println(ArrayUtil.findMin(intArray));
 
