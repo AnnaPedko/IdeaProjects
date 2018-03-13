c30775dcd87e7b97a2bbfa610a80ed0d4d8039dc
Anna Khorolets <andreevna.ne@gmail.com>
2018-03-13 02:48:44 +0200

    Added quick sort and partly merge sort algorithm

--- Task_1/src/main/java/ArrayUtil.java
+++ Task_1/src/main/java/ArrayUtil.java
@@ -4,25 +4,28 @@ public class ArrayUtil
 {
     public static void bubbleSort(int[] array)
     {
-        for ( int i = array.length -1; i > 0 ; --i )
+        if(array.length != 0)
         {
-            for ( int j = 0; j < i; j++ )
+            for (int i = array.length - 1; i > 0; --i)
             {
-                if ( array[j] > array[j+1] )
+                for (int j = 0; j < i; j++)
                 {
-                    swapArray(array, j,j+1);
+                    if (array[j] > array[j + 1])
+                    {
+                        swapArray(array, j, j + 1);
+                    }
                 }
             }
-        }
 
         System.out.println(Arrays.toString(array));
+        }
     }
 
     public static void swapArray(int[] arr, int i, int j)
     {
-        arr[i] = arr[i] + arr[j];
-        arr[j] = arr[i] - arr[j];
-        arr[i] = arr[i] - arr[j];
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static int findMax(int[] array)
@@ -49,4 +52,83 @@ public class ArrayUtil
         }
 
         return minElement;
-    }}
+    }
+
+    public static void quickSort(int[] array)
+    {
+      quickSortArray(array, 0, array.length);
+      System.out.println(Arrays.toString(array));
+    }
+
+    public static void mergeSort(int[] array)
+    {
+       mergeSortArray(array, 0, array.length);
+       System.out.println(Arrays.toString(array));
+    }
+
+    private static void mergeSortArray(int[]array, int startIndex, int finishIndex)
+    {
+        if (startIndex < finishIndex)
+        {
+            int mediana = startIndex + (finishIndex - startIndex) / 2;
+            mergeSortArray(array, 0, mediana - 1);
+            mergeSortArray(array, mediana , finishIndex);
+
+            merge(array, startIndex, mediana, finishIndex);
+
+        }
+    }
+
+    private static void merge(int[] array, int startIndex, int mediana, int finishIndex)
+    {
+        int leftSubArrayLength =  mediana - startIndex + 1;
+        int rightSubArrayLength= finishIndex - mediana;
+
+        int [] leftSubArray = new int[leftSubArrayLength];
+        int [] rightSubArray = new int[rightSubArrayLength];
+
+        for (int i = 0; i < leftSubArrayLength; i++)
+        {
+            leftSubArray[i] = array[i];
+        }
+
+        for (int j = 0; j < rightSubArrayLength; j++)
+        {
+            rightSubArray[j] = array[mediana + 1 + j];
+        }
+
+
+
+    }
+    private static void quickSortArray( int[] array, int startIndex, int finishIndex)
+    {
+        if (startIndex < finishIndex )
+        {
+            int partitionIndex = getPartitionIndex(array, 0, finishIndex);
+            quickSortArray(array, 0, partitionIndex - 1);
+            quickSortArray(array, partitionIndex + 1, finishIndex);
+        }
+    }
+
+    private static int getPartitionIndex(int[] array, int startIndex, int finishIndex)
+    {
+        int pivotIndex = finishIndex - 1;
+        int pivotElement = array[pivotIndex];
+        int partition = startIndex;
+
+        for (int j = startIndex; j < finishIndex - 1; j++)
+        {
+            if (array[j] <= pivotElement)
+            {
+                swapArray(array, partition, j);
+                partition++;
+            }
+        }
+        swapArray(array, partition, pivotIndex );
+
+        return partition;
+    }
+
+
+
+}
--- Task_1/src/main/java/Task_1.java
+++ Task_1/src/main/java/Task_1.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
+
 public class Task_1
 {
     public static void main(String[] args)
     {
         int intArray[] = {1000, -4, 12, 2, 3 , 10, 50, 7};
 
-        ArrayUtil.bubbleSort(intArray);
+        ArrayUtil.mergeSort(intArray);
+        //ArrayUtil.bubbleSort(intArray);
+       // ArrayUtil.quickSort(intArray);
         System.out.println(ArrayUtil.findMax(intArray));
         System.out.println(ArrayUtil.findMin(intArray));
 
