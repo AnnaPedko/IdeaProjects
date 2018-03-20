da3d7fde4d97157f28aad7e42bf4ee8d58d6618d
Anna Khorolets <andreevna.ne@gmail.com>
2018-03-20 02:06:26 +0200

    Added mergeSort

--- Task_1/src/main/java/ArrayUtil.java
+++ Task_1/src/main/java/ArrayUtil.java
@@ -2,6 +2,11 @@ import java.util.Arrays;
 
 public class ArrayUtil
 {
+    /**
+     * Sort a given array using BubbleSort algorithm
+     *
+     * @param array array which should be sorted ;
+     **/
     public static void bubbleSort(int[] array)
     {
         if(array.length != 0)
@@ -16,18 +21,15 @@ public class ArrayUtil
                     }
                 }
             }
-
-        System.out.println(Arrays.toString(array));
         }
     }
 
-    public static void swap(int[] arr, int i, int j)
-    {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
+    /**
+     * Find maximum element in a given array
+     *
+     * @param array array of element ;
+     * @return      max element in array
+     **/
     public static int findMax(int[] array)
     {
         int maxElement = array[0];
@@ -41,6 +43,12 @@ public class ArrayUtil
         return maxElement;
     }
 
+    /**
+     * Find minimum element in a given array
+     *
+     * @param array array of element ;
+     * @return      min element in array
+     **/
     public static int findMin(int[] array)
     {
         int minElement = array[0];
@@ -54,57 +62,51 @@ public class ArrayUtil
         return minElement;
     }
 
+    /**
+     * Find an index of searching element using a BinarySearch algorithm
+     *
+     * @param array sorted array which contains element ;
+     * @param startIndex the start index in the searching range
+     * @param endIndex the endIndex in the searching range;
+     * @param element the searching element
+     *
+     * @return index of searching element
+     **/
+    public static int binarySearch( int[] array, int startIndex, int endIndex, int element)
+    {
+        int enterIndex = startIndex + (endIndex - startIndex)/2;
+        int arrayElement = array[enterIndex];
+
+        if (element != arrayElement)
+        {
+            if (element > arrayElement)
+            {
+                enterIndex = binarySearch(array, enterIndex, endIndex, element);
+            } else {
+                enterIndex = binarySearch(array, startIndex, enterIndex, element);
+            }
+        }
+
+        return enterIndex;
+    }
+
+    /**
+     * Sort a given array using QuickSort algorithm
+     *
+     * @param array Array which should be sorted
+     **/
     public static void quickSort(int[] array)
     {
-      quickSortArray(array, 0, array.length);
-      System.out.println(Arrays.toString(array));
-    }
-
-    public static void mergeSort(int[] array)
-    {
-       mergeSortArray(array, 0, array.length);
-       System.out.println(Arrays.toString(array));
-    }
-
-    private static void mergeSortArray(int[]array, int startIndex, int finishIndex)
-    {
-        if (startIndex < finishIndex)
-        {
-            int mediana = startIndex + (finishIndex - startIndex) / 2;
-            mergeSortArray(array, 0, mediana - 1);
-            mergeSortArray(array, mediana , finishIndex);
-
-            merge(array, startIndex, mediana, finishIndex);
-
-        }
-    }
-
-    private static void merge(int[] array, int startIndex, int mediana, int finishIndex)
-    {
-        int leftSubArrayLength =  mediana - startIndex + 1;
-        int rightSubArrayLength= finishIndex - mediana;
-
-        int [] leftSubArray = new int[leftSubArrayLength];
-        int [] rightSubArray = new int[rightSubArrayLength];
-
-        for (int i = 0; i < leftSubArrayLength; i++)
-        {
-            leftSubArray[i] = array[i];
-        }
-
-        for (int j = 0; j < rightSubArrayLength; j++)
-        {
-            rightSubArray[j] = array[mediana + 1 + j];
-        }
+      quickSortArray(array, 0, array.length );
     }
 
     private static void quickSortArray( int[] array, int startIndex, int finishIndex)
     {
         if (startIndex < finishIndex )
         {
-            int partitionIndex = getPartitionIndex(array, 0, finishIndex);
-            quickSortArray(array, 0, partitionIndex - 1);
-            quickSortArray(array, partitionIndex + 1, finishIndex);
+            int partitionIndex = getPartitionIndex(array, startIndex, finishIndex);
+            quickSortArray(array, startIndex, partitionIndex - 1);
+            quickSortArray(array, partitionIndex + 1, finishIndex );
         }
     }
 
@@ -122,8 +124,93 @@ public class ArrayUtil
                 partition++;
             }
         }
+
         swap(array, partition, pivotIndex );
 
         return partition;
+    }
+
+    /**
+     * Sort a given array using MergeSort algorithm
+     *
+     * @param array Array which should be sorted ;
+     **/
+    public static void mergeSort(int[] array)
+    {
+       mergeSortArray(array, 0, array.length - 1);
+    }
+
+    private static void mergeSortArray(int[]array, int startIndex, int finishIndex)
+    {
+        if (startIndex < finishIndex) {
+
+            int mediana = startIndex + (finishIndex - startIndex) / 2;
+
+            //System.out.println("First recursion -> " + "Start Index = " + startIndex + " Mediana = " + mediana + " finishIndex  = " + finishIndex);
+            mergeSortArray(array, startIndex, mediana);
+
+            //System.out.println("Second recursion -> " + "Start Index = " + startIndex+" Mediana = " + mediana + " finishIndex  = " + finishIndex);
+            mergeSortArray(array, mediana + 1, finishIndex);
+
+            merge(array, startIndex, mediana, finishIndex);
+        }
+    }
+
+    private static void merge(int[] array, int startIndex, int mediana, int finishIndex)
+    {
+        int leftSubArrayLength =  mediana - startIndex + 1;
+        int rightSubArrayLength= finishIndex - mediana;
+
+        int [] leftSubArray = new int[leftSubArrayLength];
+        int [] rightSubArray = new int[rightSubArrayLength];
+
+        for (int i = 0; i < leftSubArrayLength; i++)
+        {
+            leftSubArray[i] = array[i + startIndex];
+        }
+
+        for (int j = 0; j < rightSubArrayLength ; j++)
+        {
+            rightSubArray[j] = array[mediana + 1 + j];
+        }
+
+        int k = startIndex;
+        int i = 0;
+        int j = 0;
+
+        while(i < leftSubArrayLength && j < rightSubArrayLength)
+        {
+            if (leftSubArray[i] < rightSubArray[j])
+            {
+                array[k] = leftSubArray[i];
+                i++;
+            } else {
+                array[k] = rightSubArray[j];
+                j++;
+            }
+
+            k++;
+        }
+
+         while(i < leftSubArrayLength)
+         {
+             array[k] = leftSubArray[i];
+             i++;
+             k++;
+         }
+
+         while(j < rightSubArrayLength)
+         {
+             array[k] = rightSubArray[j];
+             j++;
+             k++;
+         }
+    }
+
+    private static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
--- Task_1/src/main/java/Task_1.java
+++ Task_1/src/main/java/Task_1.java
@@ -1,15 +1,125 @@
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class Task_1
+interface SortAlgorithm {
+    void sort(int[] array);
+    String getName();
+}
+
+abstract class AlgorithmName implements SortAlgorithm {
+    AlgorithmName(String name)
+    {
+       name_ = name;
+    }
+
+    @Override
+    public String getName()
+    {
+        return name_;
+    }
+    private String name_;
+}
+
+class BubbleSort extends AlgorithmName
 {
+    BubbleSort()
+    {
+        super("BubbleSort");
+    }
+
+    @Override
+    public void sort(int[] array)
+    {
+        ArrayUtil.bubbleSort(array);
+    }
+}
+
+class MergeSort extends AlgorithmName
+{
+    MergeSort()
+    {
+        super("MergeSort");
+    }
+
+    @Override
+    public void sort(int[] array)
+    {
+        ArrayUtil.mergeSort(array);
+    }
+}
+
+class QuickSort extends AlgorithmName
+{
+    QuickSort()
+    {
+        super("QuickSort");
+    }
+
+    @Override
+    public void sort(int[] array)
+    {
+        ArrayUtil.quickSort(array);
+    }
+}
+
+class SortApplier{
+    public static void applySort(SortAlgorithm algorithm, int[] array)
+    {
+        System.out.println("Array BEFORE applying " + algorithm.getName() + " Algo" + Arrays.toString(array));
+        algorithm.sort(array);
+        System.out.println("Array AFTER applying " + algorithm.getName() +" Algo" + Arrays.toString(array));
+    }
+}
+
+public class Task_1 {
+
     public static void main(String[] args)
     {
-        int intArray[] = {1000, -4, 12, 2, 3 , 10, 50, 7};
-        int quickSortArray[] = {1000, -4, 10, 92, 3 , 11, 50, 7};
-        //ArrayUtil.mergeSort(intArray);
-        ArrayUtil.bubbleSort(intArray);
-        ArrayUtil.quickSort(quickSortArray);
-        System.out.println(ArrayUtil.findMax(intArray));
-        System.out.println(ArrayUtil.findMin(intArray));
+        int arraySize = 8;
+        int binaryArray[] = {1, 2, 4, 6, 8, 10, 20, 1000};
+
+        //Binary search
+       int findElement = binaryArray[random(0, binaryArray.length - 1)];
+       int indexElement = ArrayUtil.binarySearch(binaryArray, 0, binaryArray.length, findElement);
+
+        System.out.println("Sorted Array for Binary Search = " + Arrays.toString(binaryArray));
+        System.out.println("Index  = " + indexElement  + " of searching element = " + findElement);
+        printDelimiter();
+
+        //BubbleSort algo
+        SortApplier.applySort(new MergeSort(), generateArray(arraySize, 0, 10));
+        printDelimiter();
+        SortApplier.applySort(new QuickSort(), generateArray(arraySize, 0, 10));
+        printDelimiter();
+        SortApplier.applySort(new BubbleSort(), generateArray(arraySize, 0, 10));
+        printDelimiter();
+
+        //find min, max in bubble sort array
+        System.out.println("Array for finding max, min = " + Arrays.toString(binaryArray));
+        System.out.println(ArrayUtil.findMax(binaryArray));
+        System.out.println(ArrayUtil.findMin(binaryArray));
     }
+
+    public static int random(int min, int max)
+    {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    public static int[] generateArray(int elementNumber, int min, int max)
+    {
+     int [] array = new int[elementNumber];
+
+         for(int i = 0; i < array.length; i++)
+         {
+            array[i] = random(min, max);
+         }
+
+     return array;
+    }
+
+    public static void printDelimiter()
+    {
+        System.out.println("----------------------------------------------");
+    }
+
 }
